6157a2c8cf1d4b29587f0abf80ae63de22c55f9f
Armando Andrés Meabe <armando.meabe@gmail.com>
2017-10-03 01:08:55 -0300

    nuevo wcf con datacontracts, datamembers e iqueryable

---
+++ dinamica.erp.wcf/dinamica.erp.wcf.arm/IService1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace dinamica.erp.wcf.arm
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        ArticuloDto GetDataUsingDataContract(ArticuloDto composite);
+
+        [OperationContract]
+        IQueryable<ArticuloDto> GetArticulos(Expression<Func<Artículos, bool>> where);
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    // Puede agregar archivos XSD al proyecto. Después de compilar el proyecto, puede usar directamente los tipos de datos definidos aquí, con el espacio de nombres "dinamica.erp.wcf.arm.ContractType".
+    [DataContract]
+    public class ArticuloDto
+    {
+        string titulo = "Hello ";
+
+        [DataMember]
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+    }
+}
---
+++ dinamica.erp.wcf/dinamica.erp.wcf.arm/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace dinamica.erp.wcf.arm
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public ArticuloDto GetDataUsingDataContract(ArticuloDto composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            return composite;
+        }
+
+        public IQueryable<ArticuloDto> GetArticulos(Expression<Func<Artículos, bool>> where) //Por que artículos acá? (tabla Artículos de EntityFX)
+        {
+            return (new ElediEntities()).Artículos.Where(where).Select(x => new ArticuloDto { Titulo = x.Título });
+    }
+}
+}
